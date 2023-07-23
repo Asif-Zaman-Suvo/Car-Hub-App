a2d423c5dacbd0f606e6e19f8787c2c8782a05a2
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2023-07-23 16:14:47 +0600

    Hero section edited and footer completed

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -7,30 +7,47 @@ const Footer = () => {
   return (
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
       <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
-        <Image
-          alt="Car hub logo"
-          src="/logo.svg"
-          className="object-contain"
-          width={118}
-          height={18}
-        />
-        <p className="text-base text-gray-700">
-          Carhub 2023 <br />
-          All rights reserved &copy;
-        </p>
-      </div>
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            alt="Car hub logo"
+            src="/logo.svg"
+            className="object-contain"
+            width={118}
+            height={18}
+          />
+          <p className="text-base text-gray-700">
+            Carhub 2023 <br />
+            All rights reserved &copy;
+          </p>
+        </div>
 
-      <div className="footer__links">
-        {footerLinks.map((link) => (
-          <div className="footer__link" key={link.title}>
-            <h3 className="font-bold">{link.title}</h3>
-            {link.links.map((item) => (
-              <Link className="text-gray-500" href={item.url} key={item.title}>
-                {item.title}
-              </Link>
-            ))}
-          </div>
-        ))}
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div className="footer__link" key={link.title}>
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  className="text-gray-500"
+                  href={item.url}
+                  key={item.title}
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2023 CarHub. All rights reserved</p>
+        <div className="footer__copyrights-link">
+          <Link className="text-gray-500" href="/">
+            Privacy Policy
+          </Link>
+          <Link className="text-gray-500" href="/">
+            Tearms of use
+          </Link>
+        </div>
       </div>
     </footer>
   );
